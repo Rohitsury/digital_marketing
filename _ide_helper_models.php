ea1c992f49ff6a0025fa3072ad9db5e29c930046
Dalpat Singh <dalpatsingh@innowaysit.com>
2021-08-26 15:28:53 +0530

    ide-helper added

---
+++ _ide_helper_models.php
@@ -0,0 +1,147 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Ad
+ *
+ * @property int $id
+ * @property int $client_id
+ * @property string $title
+ * @property string $description
+ * @property string $banner
+ * @property string $platform
+ * @property string $start_date
+ * @property string $end_date
+ * @property string $status
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereBanner($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereClientId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereEndDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad wherePlatform($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereStartDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Ad whereUpdatedAt($value)
+ */
+	class Ad extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Client
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property int|null $mobile
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $address
+ * @property string $type
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Client newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereMobile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Client whereUpdatedAt($value)
+ */
+	class Client extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Staff
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property int|null $mobile
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string|null $address
+ * @property string $type
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereMobile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Staff whereUpdatedAt($value)
+ */
+	class Staff extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property int|null $mobile
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $address
+ * @property string $type
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereMobile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
