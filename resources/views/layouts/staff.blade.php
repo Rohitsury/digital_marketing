d0f1518394a24849dbcd5d7a0abc2385cdc8044b
Dalpat Singh <dalpatsingh@innowaysit.com>
2021-09-01 17:10:20 +0530

    titles updated

--- resources/views/layouts/staff.blade.php
+++ resources/views/layouts/staff.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>@yield('title')</title>
+    <title>@yield('title', 'Staff Portal')</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('sbadmin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
